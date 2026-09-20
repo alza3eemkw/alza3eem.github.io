a249c8a3ea1d1d5ea54d7b37054989bddf0766ad
alza3eem <A7ayat2009@icloud.com>
2026-09-20 21:26:41 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,73 @@
+const CACHE_NAME = 'alzaeem-pwa-v1';
+
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  const requestURL = new URL(event.request.url);
+
+  // لا نتدخل بالملفات الخارجية مثل Google Fonts
+  if (requestURL.origin !== self.location.origin) return;
+
+  // عند فتح الموقع: حاول جلب أحدث نسخة من الإنترنت
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put('./index.html', copy));
+
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+
+    return;
+  }
+
+  // الملفات المحلية: استخدم النسخة المخزنة إن وجدت
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+
+        if (response.ok) {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, copy));
+        }
+
+        return response;
+      });
+    })
+  );
+});
